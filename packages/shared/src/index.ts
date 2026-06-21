35007b8c3cae191dab6b9441cbfac3b21df4c82d
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2026-06-21 12:19:36 +0300

    Create shared packages

---
+++ packages/shared/src/index.ts
@@ -0,0 +1,26 @@
+import { isDefined, isEmpty, isPositive } from "class-validator";
+
+export abstract class RequestDto {
+    abstract validate(): string[];
+}
+
+//===================== LANGUAGE ==========================
+
+export class CreateLanguage extends RequestDto {
+    name!: string;
+    abbrev!: string;
+
+    constructor(lang: Partial<CreateLanguage>) {
+        super();
+        Object.assign(this, lang);
+    }
+
+    validate(): string[] {
+        const errors: string[] = [];
+
+        if(isEmpty(this.name)) errors.push("Name is missing");
+        if(isEmpty(this.abbrev)) errors.push("Abbreviation is missing");
+
+        return errors;
+    }
+}
