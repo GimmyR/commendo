4bc0e85af3c3b8cb8fb93f4d3f6b9f9676077641
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2026-06-21 21:37:35 +0300

    Import bootstrap icons

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.min.css';
 import Home from './components/home';
 import Layout from './components/layout';
 
