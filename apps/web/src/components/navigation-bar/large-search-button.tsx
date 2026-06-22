30ae02c4f134fbaf7a87a8bdb299fc1219d38f30
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2026-06-22 08:36:37 +0300

    Rename button title

--- apps/web/src/components/navigation-bar/large-search-button.tsx
+++ apps/web/src/components/navigation-bar/large-search-button.tsx
@@ -3,7 +3,7 @@ import { Button } from "react-bootstrap";
 export default function LargeSearchButton() {
     return (
         <Button variant="outline-light" className="d-none d-md-inline col-md-5 col-lg-4 me-4 text-start rounded-5 cmd-btn">
-            <i className="bi bi-search me-2"></i>Search
+            <i className="bi bi-search me-2"></i>Rechercher
         </Button>
     );
 }
