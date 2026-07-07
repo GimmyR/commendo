c023ef3357a87353f89b36603c63dba683ed93c5
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2026-07-07 15:28:07 +0300

    Add translation for sign-in/sign-out button in user button

--- apps/web/src/components/navigation-bar/user-button/sign-in-button.tsx
+++ apps/web/src/components/navigation-bar/user-button/sign-in-button.tsx
@@ -1,13 +1,15 @@
 import { Button } from "react-bootstrap";
 import useSignInButton from "@/libs/hooks/use-sign-in-button";
 import SignInModal from "./sign-in-modal";
+import { useTranslation } from "react-i18next";
 
 export default function SignInButton() {
     const {isShown, showSignIn, closeSignIn} = useSignInButton();
+    const { t } = useTranslation("userButtonMenu");
 
     return (
         <>
-            <Button variant="success" onClick={showSignIn} className="col-12">Connexion</Button>
+            <Button variant="success" onClick={showSignIn} className="col-12">{t("sign-in")}</Button>
             <SignInModal isShown={isShown} close={closeSignIn}/>
         </>
     );
--- apps/web/src/components/navigation-bar/user-button/sign-out-button.tsx
+++ apps/web/src/components/navigation-bar/user-button/sign-out-button.tsx
@@ -1,13 +1,15 @@
 import SignOutModal from "@/components/navigation-bar/user-button/sign-out-modal";
 import useSignOutButton from "@/libs/hooks/use-sign-out-button";
 import { Button } from "react-bootstrap";
+import { useTranslation } from "react-i18next";
 
 export default function SignOutButton() {
     const {isShown, showSignOut, closeSignOut} = useSignOutButton();
+        const { t } = useTranslation("userButtonMenu");
 
     return (
         <>
-            <Button variant="danger" onClick={showSignOut} className="col-12">Déconnexion</Button>
+            <Button variant="danger" onClick={showSignOut} className="col-12">{t("sign-out")}</Button>
             <SignOutModal isShown={isShown} close={closeSignOut}/>
         </>
     );
