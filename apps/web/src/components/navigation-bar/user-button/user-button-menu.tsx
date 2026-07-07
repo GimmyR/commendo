e1f07549c63067696b022c649629c43bb9d4d02c
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2026-07-07 15:24:35 +0300

    Add translation for settings in user button

--- apps/web/src/components/navigation-bar/user-button/user-button-menu.tsx
+++ apps/web/src/components/navigation-bar/user-button/user-button-menu.tsx
@@ -4,8 +4,11 @@ import SignInButton from "./sign-in-button";
 import SignedOut from "@/components/signed-out";
 import SignedIn from "@/components/signed-in";
 import SignOutButton from "@/components/navigation-bar/user-button/sign-out-button";
+import { useTranslation } from "react-i18next";
 
 export default function UserButtonMenu() {
+    const { t } = useTranslation("userButtonMenu");
+
     return (
         <Dropdown.Menu>
             <Dropdown.Item className="cmd-dropdown-item" onClick={(e) => e.stopPropagation()}>
@@ -13,7 +16,7 @@ export default function UserButtonMenu() {
             </Dropdown.Item>
             <SignedIn>
                 <Dropdown.Divider/>
-                <Dropdown.Item className="text-center">Paramètres</Dropdown.Item>
+                <Dropdown.Item className="text-center">{t("settings")}</Dropdown.Item>
                 <Dropdown.Divider/>
                 <Dropdown.Item className="cmd-dropdown-item">
                     <SignOutButton/>
--- apps/web/src/i18n.ts
+++ apps/web/src/i18n.ts
@@ -2,15 +2,19 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import frLargeSearchButton from "./locales/fr/large-search-button.json";
 import engLargeSearchButton from "./locales/eng/large-search-button.json";
+import frUserButtonMenu from "./locales/fr/user-button-menu.json";
+import engUserButtonMenu from "./locales/eng/user-button-menu.json";
 
 i18n.use(initReactI18next)
     .init({
         resources: {
             fr: {
-                largeSearchButton: frLargeSearchButton
+                largeSearchButton: frLargeSearchButton,
+                userButtonMenu: frUserButtonMenu
             },
             eng: {
-                largeSearchButton: engLargeSearchButton
+                largeSearchButton: engLargeSearchButton,
+                userButtonMenu: engUserButtonMenu
             }
         },
         fallbackLng: "fr",
