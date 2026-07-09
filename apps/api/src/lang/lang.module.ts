05a404b1652125e83486d9e0940a2004918da81f
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2026-07-09 15:34:53 +0300

    Remove RoleService import

--- apps/api/src/lang/lang.module.ts
+++ apps/api/src/lang/lang.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { LangController } from './lang.controller';
 import { LangService } from './lang.service';
 import { AccountService } from 'src/account/account.service';
-import { RoleService } from 'src/role/role.service';
 
 @Module({
   controllers: [LangController],
-  providers: [LangService, AccountService, RoleService]
+  providers: [LangService, AccountService]
 })
 export class LangModule {}
