948828b80cae33f2339898798fa9de5e4cf733ed
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2026-07-10 15:27:07 +0300

    Create API doc for GET endpoint /api/dish

--- apps/api/src/dish/dish.controller.ts
+++ apps/api/src/dish/dish.controller.ts
@@ -1,13 +1,21 @@
 import { DishService } from '@/dish/dish.service';
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Query } from '@nestjs/common';
+import { ApiOperation, ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller('dish')
+@ApiTags('dish')
 export class DishController {
     constructor(
         private readonly dishServ: DishService
     ) {}
 
     @Get()
+    @ApiOperation({ summary: "Find all dishes" })
+    @ApiQuery({ name: "lang", required: true, type: String, example: "fr" })
+    @ApiQuery({ name: "page", required: false, type: Number, example: 1 })
+    @ApiQuery({ name: "limit", required: false, type: Number, example: 10 })
+    @ApiResponse({ status: HttpStatus.OK, description: "Dishes have been successfully returned" })
+    @ApiResponse({ status: HttpStatus.INTERNAL_SERVER_ERROR, description: "Unknown error" })
     async findAll(
         @Query("lang") language: string, 
         @Query("page") page: number, 
