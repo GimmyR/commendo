a8b14c7b53e6cd9ab4a5bbc85fca2a41302bce7e
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2026-07-11 15:42:49 +0300

    Change style when hovering dish

--- apps/web/src/components/dishes/dish/index.tsx
+++ apps/web/src/components/dishes/dish/index.tsx
@@ -1,13 +1,19 @@
 import { CURRENCY } from "@/libs/utils/constants";
 import type { DishWithIngredients } from "@repo/shared";
+import { useState } from "react";
 
 type Props = {
     dish: DishWithIngredients
 };
 
 export default function Dish({ dish } : Props) {
+    const [active, setActive] = useState<boolean>(false);
+
+    const handleHover = () => setActive(true);
+    const handleLeave = () => setActive(false);
+
     return (
-        <div className="p-3 border border-2">
+        <div className={`p-3 border border-2 ${active && 'shadow'}`} onMouseEnter={handleHover} onMouseLeave={handleLeave}>
             <div className="border" style={{ height: "200px" }}></div>
             <div className="pt-3 pb-2">
                 <div className="d-flex flex-row justify-content-center">
