0bdb78f5fbeacfda679940200e0b8ebfe956ae94
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2026-07-12 08:07:22 +0300

    Make unit test on Dishes without testing pagination

---
+++ apps/web/src/components/dishes/index.spec.tsx
@@ -0,0 +1,42 @@
+import Dishes from "@/components/dishes";
+import { fetchAllDishesWithIngredients } from "@/libs/actions/dish";
+import type { DishWithIngredients } from "@repo/shared";
+import { render, screen, waitFor } from "@testing-library/react";
+
+const dish: DishWithIngredients = {
+    id: 1,
+    active: true,
+    price: 3000,
+    names: [
+        { langId: 1, name: "Poulet roti" }
+    ],
+    ingredients: []
+};
+
+vi.mock('@/libs/actions/dish', () => {
+    return {
+        fetchAllDishesWithIngredients: vi.fn()
+    };
+});
+
+describe("Test Dishes", () => {
+    beforeEach(() => {
+        vi.restoreAllMocks();
+    });
+
+    it("Should display dishes", async () => {
+        vi.mocked(fetchAllDishesWithIngredients).mockResolvedValue({
+            data: [dish],
+            pages: 1,
+            total: 1
+        });
+
+        render(<Dishes/>);
+        await waitFor(() => {
+            const name = screen.getByText(dish.names[0].name);
+            expect(name).toBeInTheDocument();
+            const price = screen.getByText(dish.price);
+            expect(price).toBeInTheDocument();
+        });
+    });
+});
