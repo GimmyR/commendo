4733c9a04a2e42edd7808f4f90e094aae98cd110
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2026-07-12 08:05:25 +0300

    Make unit test on DishItem

---
+++ apps/web/src/components/dishes/dish-item.spec.tsx
@@ -0,0 +1,25 @@
+import DishItem from "@/components/dishes/dish-item";
+import type { DishWithIngredients } from "@repo/shared";
+import { render, screen } from "@testing-library/react";
+
+const dish: DishWithIngredients = {
+    id: 1,
+    active: true,
+    price: 3000,
+    names: [
+        { langId: 1, name: "Poulet roti" }
+    ],
+    ingredients: []
+};
+
+const handleClick = () => {};
+
+describe("Test DishItem", () => {
+    it("Should display dish informations", () => {
+        render(<DishItem dish={dish} onClick={handleClick}/>);
+        const name = screen.getByText(dish.names[0].name);
+        expect(name).toBeInTheDocument();
+        const price = screen.getByText(dish.price);
+        expect(price).toBeInTheDocument();
+    });
+});
