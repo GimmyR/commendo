571492a6c31857cffc3f24cd8607ba3de9774329
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2026-07-17 08:30:14 +0300

    Serve (API) and display image (Front)

--- front/src/components/dishes/dish-item.tsx
+++ front/src/components/dishes/dish-item.tsx
@@ -1,5 +1,5 @@
 import type { DishWithIngredients } from "@/libs/actions/dishes";
-import { CURRENCY } from "@/libs/utils/constants";
+import { API_URL, CURRENCY } from "@/libs/utils/constants";
 import { useState } from "react";
 
 type Props = {
@@ -9,13 +9,18 @@ type Props = {
 
 export default function DishItem({ dish, onClick } : Props) {
     const [active, setActive] = useState<boolean>(false);
+    const imageStyle: React.CSSProperties = { 
+        backgroundImage: `url(${API_URL}/resource/${dish.image})`,
+        backgroundPosition: "center",
+        backgroundSize: "cover"
+    };
 
     const handleHover = () => setActive(true);
     const handleLeave = () => setActive(false);
 
     return (
         <div className={`p-3 border border-2 ${active && 'shadow'}`} onClick={onClick} onMouseEnter={handleHover} onMouseLeave={handleLeave}>
-            <div className="border" style={{ height: "200px" }}></div>
+            <div className="border" style={{ height: "200px", ...imageStyle }}></div>
             <div className="pt-3 pb-2">
                 <div className="d-flex flex-row justify-content-center">
                     <strong className="text-success text-truncate" title={dish.names[0].name}>
