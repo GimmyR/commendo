a635e4ff72b7ac731a24c1fba441cfb543cb3016
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2026-08-07 09:54:55 +0300

    Make column wider

--- front/src/components/orders/orders-column.tsx
+++ front/src/components/orders/orders-column.tsx
@@ -22,7 +22,7 @@ export default function OrdersColumn({ group, status, orders } : Props) {
     const { t } = useTranslation("orders");
 
     return (
-        <Col className="col-8 col-md-4 col-xxl-3 pt-4 pt-md-0">
+        <Col className="col-10 col-md-4 col-xxl-3 pt-4 pt-md-0">
             <Stack direction="vertical" className="pb-2 border">
                 <h1 className={`fs-4 text-center text-dark border-bottom border-4 border-${status.color} py-2`}>{t(status.key)}</h1>
                 <Stack ref={ref} direction="vertical" gap={3} className="px-3 py-2" style={{ minHeight: "50px" }}>
