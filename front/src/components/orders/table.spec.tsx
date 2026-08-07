426761249713129878bf536178218f9607511d55
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2026-08-07 11:09:34 +0300

    Remove test for select order status and verify if status is displayed in the table

--- front/src/components/orders/table.spec.tsx
+++ front/src/components/orders/table.spec.tsx
@@ -27,13 +27,13 @@ const order: OrderWithTableAndDish = {
 
 describe("Test OrdersTable", () => {
     it("Should not display table of orders", () => {
-        render(<OrdersTable orders={[]} changeStatus={vi.fn()}/>);
+        render(<OrdersTable orders={[]}/>);
         const title = screen.getByText("Aucune commande");
         expect(title).toBeInTheDocument();
     });
 
     it("Should display table of orders", () => {
-        render(<OrdersTable orders={[order]} changeStatus={vi.fn()}/>);
+        render(<OrdersTable orders={[order]}/>);
         const tableColumn = screen.getByText("Table");
         expect(tableColumn).toBeInTheDocument();
         const dishColumn = screen.getByText("Plat");
@@ -44,21 +44,7 @@ describe("Test OrdersTable", () => {
         expect(tableRef).toBeInTheDocument();
         const dishName = screen.getByText(order.dish.names[0].name);
         expect(dishName).toBeInTheDocument();
-    });
-
-    it("Should display select with all states", () => {
-        render(<OrdersTable orders={[order]} changeStatus={vi.fn()}/>);
-        const status1 = screen.getByText("A confirmer");
-        expect(status1).toBeInTheDocument();
-        const status2 = screen.getByText("A faire");
-        expect(status2).toBeInTheDocument();
-        const status3 = screen.getByText("En préparation");
-        expect(status3).toBeInTheDocument();
-        const status4 = screen.getByText("Terminée");
-        expect(status4).toBeInTheDocument();
-        const status5 = screen.getByText("Annulée");
-        expect(status5).toBeInTheDocument();
-        const status6 = screen.getByText("Archivée");
-        expect(status6).toBeInTheDocument();
+        const orderStatus = screen.getByText("A faire");
+        expect(orderStatus).toBeInTheDocument();
     });
 });
